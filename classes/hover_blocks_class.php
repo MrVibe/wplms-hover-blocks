17927345389b1dcea979e398d84e75479ef780ea
megha <megha@99fusion.com>
2015-06-20 19:35:56 +0530

    slide on hover plugin code

---
+++ classes/hover_blocks_class.php
@@ -0,0 +1,90 @@
+<?php
+
+if(!class_exists('WPLMS_HoverBlock_Plugin_Class'))
+{   
+    class WPLMS_HoverBlock_Plugin_Class  // We'll use this just to avoid function name conflicts 
+    {
+            
+        public function __construct(){   
+      
+ 		add_filter('vibe_builder_thumb_styles',array($this,'custom_vibe_builder_thumb_styles'));
+    	add_filter('vibe_featured_thumbnail_style',array($this,'hover_vibe_featured_thumbnail_style'),10,3);
+      	add_action('wplms_customizer_custom_css',array($this,'customize_color'),10,1);
+        } // END public function __construct
+        function custom_vibe_builder_thumb_styles($thumb_array){
+            $thumb_array['slide_info']= 'http://localhost/wplms/wp-content/plugins/wplms-hover-blocks/images/hover_img.jpg';
+            return $thumb_array;
+        }
+        function hover_vibe_featured_thumbnail_style($thumbnail_html,$post,$style){
+		    if($style == 'slide_info'){ //Custom block is the same name as added for the thumbnail in pagebuilder
+		         $thumbnail_html ='<div class="block slide_info">';
+		        $thumbnail_html .= '<div class="block_media">';
+		        $thumbnail_html .= '<a href="'.get_permalink($post->ID).'">'.get_the_post_thumbnail($post->ID,'medium').'</a>';
+		        $thumbnail_html .= '</div>';
+		        $thumbnail_html .= '<div class="block_content">';
+		        $thumbnail_html .= apply_filters('vibe_thumb_heading','<h4 class="block_title"><a href="'.get_permalink($post->ID).'" title="'.$post->post_title.'">'.$post->post_title.'</a></h4>',$featured_style);
+
+                if(get_post_type($post->ID) == 'course'){
+                    $rating=get_post_meta($post->ID,'average_rating',true);
+                    $rating_count=get_post_meta($post->ID,'rating_count',true);
+                    $meta = '<div class="star-rating">';
+                    for($i=1;$i<=5;$i++){
+
+                        if(isset($rating)){
+                            if($rating >= 1){
+                                $meta .='<span class="fill"></span>';
+                            }elseif(($rating < 1 ) && ($rating > 0.4 ) ){
+                                $meta .= '<span class="half"></span>';
+                            }else{
+                                $meta .='<span></span>';
+                            }
+                            $rating--;
+                        }else{
+                            $meta .='<span></span>';
+                        }
+                    }
+                    $meta =  apply_filters('vibe_thumb_rating',$meta,$featured_style,$rating);
+                    $meta .= apply_filters('vibe_thumb_reviews','(&nbsp;'.(isset($rating_count)?$rating_count:'0').'&nbsp;'.__('REVIEWS','vibe-customtypes').'&nbsp;)',$featured_style).'</div>';
+
+                    $free_course = get_post_meta($post->ID,'vibe_course_free',true);
+
+                    $meta .=bp_course_get_course_credits(array('id'=>$post->ID));
+                    
+                    $meta .='<span class="clear"></span>';
+
+                    
+                    $instructor_meta='';
+                    if(function_exists('bp_course_get_instructor'))
+                        $instructor_meta .= bp_course_get_instructor();
+                    
+                    $meta .= apply_filters('vibe_thumb_instructor_meta',$instructor_meta,$featured_style);
+
+                    $st = get_post_meta($post->ID,'vibe_students',true);
+                    if(isset($st) && $st !='')
+                        $meta .= apply_filters('vibe_thumb_student_count','<strong>'.$st.' '.__('Students','vibe-customtypes').'</strong>');
+
+                    
+                    $thumbnail_html .= $meta;
+                }
+		        $thumbnail_html .= '</div>';
+		        $thumbnail_html .= '</div>';
+		    }
+		    return $thumbnail_html;
+		}
+	
+
+
+		function customize_color($customizer){
+			if(isset($customizer['single_dark_color'])){
+	          echo '.block.slide_info .block_content{background: '.$customizer['single_dark_color'].' !important;}';
+	        }
+		}
+                           
+
+         // ADD custom Code in clas
+        
+    } // END class WPLMS_HoverBlock_Plugin_Class
+} // END if(!class_exists('WPLMS_HoverBlock_Plugin_Class'))
+
+new  WPLMS_HoverBlock_Plugin_Class;
+?>
--- wplms_hover_blocks.php
+++ wplms_hover_blocks.php
@@ -8,3 +8,40 @@ Author: VibeThemes
 Author URI: http://www.vibethemes.com
 License: GPL2
 */
+
+include_once 'classes/hover_blocks_class.php';
+
+if(class_exists('WPLMS_HoverBlock_Plugin_Class'))
+{	
+    // Installation and uninstallation hooks
+    register_activation_hook(__FILE__, array('WPLMS_HoverBlock_Plugin_Class', 'activate'));
+    register_deactivation_hook(__FILE__, array('WPLMS_HoverBlock_Plugin_Class', 'deactivate'));
+
+    // instantiate the plugin class
+    $wplms_customizer = new WPLMS_Customizer_Plugin_Class();
+}
+
+function wplms_hover_enqueue_scripts(){
+    wp_enqueue_style( 'wplms-hover1-css', plugins_url( 'css/component.css' , __FILE__ ));
+    wp_enqueue_style( 'wplms-hover2-css', plugins_url( 'css/default.css' , __FILE__ ));
+    wp_enqueue_script( 'wplms-hover1-js', plugins_url( 'js/modernizr.custom.js' , __FILE__ ));
+     wp_enqueue_script( 'wplms-hover2-js', plugins_url( 'js/toucheffects.js' , __FILE__ ));
+}
+
+add_action('wp_head','wplms_hover_enqueue_scripts');
+
+add_action('wp_enqueue_scripts','wplms_hover_custom_cssjs');
+
+/**
+ * Objective: Register & Enqueue your Custom scripts
+ * Developer notes:
+ * Hook you custom scripts required for the plugin here.
+ */
+function wplms_hover_custom_cssjs(){
+    wp_enqueue_style( 'wplms-hover-css', plugins_url( 'css/component.css' , __FILE__ ));
+    wp_enqueue_style( 'wplms-hover-css', plugins_url( 'css/default.css' , __FILE__ ));
+    wp_enqueue_script( 'wplms-hover-js', plugins_url( 'js/modernizr.custom.js' , __FILE__ ));
+     wp_enqueue_script( 'wplms-hover-js', plugins_url( 'js/toucheffects.js' , __FILE__ ));
+}
+
+?>
