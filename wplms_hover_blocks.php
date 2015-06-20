ac76004198912b7c9c24f17bab8607ec114e68d7
megha <megha@99fusion.com>
2015-06-20 15:52:21 +0530

    Init 0.1

---
+++ wplms_hover_blocks.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: WPLMS Hover Blocks
+Plugin URI: http://www.Vibethemes.com
+Description: WordPress plugin to add new fured blocks in WPLMS
+Version: 1.0
+Author: VibeThemes
+Author URI: http://www.vibethemes.com
+License: GPL2
+*/
